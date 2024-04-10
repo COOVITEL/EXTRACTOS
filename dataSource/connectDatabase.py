adbe03b06c9fd74a30d29c1aa87e62ec56b6ef3c
COOVITEL <direcciontop@coovitel.coop>
2024-04-10 16:59:59 -0500

    update and connect with database

--- dataSource/connectDatabase.py
+++ dataSource/connectDatabase.py
@@ -1,10 +1,13 @@
 import cx_Oracle
+import pandas as pd
+from decrypt import decrypt
 
-ip = '192.168.1.12'
+ip = '192.168.1.139'
 port = '1521'
-service = 'LINIX'
-username = 'VISTAS'
-userpassword = 'VISTAS'
+service = decrypt('krfojra', 'valkiria')
+username = decrypt('nqcpb', 'ciphernewman')
+userpassword = decrypt('gqtmk', 'vigenerepython')
+
 
 # Configuración de la conexión
 dsn_tns = cx_Oracle.makedsn(ip, port, service_name=service)
@@ -24,9 +27,34 @@ except cx_Oracle.DatabaseError as e:
 if conn:
     try:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM <TU_TABLA>')
-        for row in cursor:
-            print(row)
+        query = """
+                    SELECT K_CUENTA K_CUENTA_AH136
+                        ,A_NUMCTA
+                        ,N_NUMCTA
+                        ,K_CLASIF
+                        ,I_ESTADO
+                        ,K_IDTERC_ASO
+                        ,pk_ah_saldo.fu_v_saldo_actual_dia
+                                            (k_cuenta,
+                                            '21050501',
+                                            k_sucurs,
+                                            :P_F_CORTE_HAS
+                                            ) SALDOFIN
+                        FROM AH136MCUENTA
+                        where i_estado = 'A'
+                """
+        cursor.execute("""
+                       SELECT CEDULA, NNASOCIA, REGIONAL, CUENTA, ESTADO FROM BI_AHORRO_VISTA_MES WHERE ESTADO IS NOT NULL
+                       """)
+                        # 1037 + CUENTA
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        
+        results = [dict(zip(columns, row)) for row in data]
+
+        
+        print(len(results))
+
     except cx_Oracle.DatabaseError as e:
         error, = e.args
         print('Error al ejecutar la consulta: %s' % e)
