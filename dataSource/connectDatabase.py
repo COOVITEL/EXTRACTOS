90f0fd16c8dd7af23996a669861025668be92149
COOVITEL <direcciontop@coovitel.coop>
2024-04-17 16:45:59 -0500

    Update dates and add querys

--- dataSource/connectDatabase.py
+++ dataSource/connectDatabase.py
@@ -1,16 +1,19 @@
-import cx_Oracle
-import pandas as pd
-from decrypt import decrypt
 from dotenv import load_dotenv
+from decrypt import decrypt
+import pandas as pd
+import cx_Oracle
 import os
 
 load_dotenv()
 
 ip = os.getenv('IP')
 port = os.getenv('PORT')
-service = decrypt(os.getenv('SERVICENAME'), os.getenv('SERVICEENCRYPT'))
-username = decrypt(os.getenv('USERNAME'), os.getenv('USERNAMEENCRYPT'))
-userpassword = decrypt(os.getenv('PASSWORD'), os.getenv('PASSWORDENCRYPT'))
+#service = decrypt(os.getenv('SERVICENAME'), os.getenv('SERVICEENCRYPT'))
+#username = decrypt(os.getenv('USERNAME'), os.getenv('USERNAMEENCRYPT'))
+#userpassword = decrypt(os.getenv('PASSWORD'), os.getenv('PASSWORDENCRYPT'))
+service = "LINIX"
+username = "VISTAS"
+userpassword = "VISTAS"
 
 
 # Configuración de la conexión
@@ -31,37 +34,22 @@ except cx_Oracle.DatabaseError as e:
 if conn:
     try:
         cursor = conn.cursor()
-        query = """
-                    SELECT K_CUENTA K_CUENTA_AH136
-                        ,A_NUMCTA
-                        ,N_NUMCTA
-                        ,K_CLASIF
-                        ,I_ESTADO
-                        ,K_IDTERC_ASO
-                        ,pk_ah_saldo.fu_v_saldo_actual_dia
-                                            (k_cuenta,
-                                            '21050501',
-                                            k_sucurs,
-                                            :P_F_CORTE_HAS
-                                            ) SALDOFIN
-                        FROM AH136MCUENTA
-                        where i_estado = 'A'
-                """
-        cursor.execute("""
-                       SELECT CEDULA, NNASOCIA, REGIONAL, CUENTA, ESTADO FROM BI_AHORRO_VISTA_MES WHERE ESTADO IS NOT NULL
-                       """)
+
+        query_users = "SELECT * FROM Ahorr_vist_encab"
+        query_movs = "SELECT * FROM Ahorr_vist_movimi"
+        cursor.execute(query_movs)
+
                         # 1037 + CUENTA
         # For you can all dates in the query  use fetchall()
-        # data = cursor.fetchall()
-        
-        data = cursor.fetchmany(20)
+        data = cursor.fetchall()
+        #data = cursor.fetchmany(3)
         columns = [desc[0] for desc in cursor.description]
         
         results = [dict(zip(columns, row)) for row in data]
 
-        
-        for result in results:
-            print(result)
+        print(len(results))
+        #for result in results:
+        #    print(result)
 
     except cx_Oracle.DatabaseError as e:
         error, = e.args
